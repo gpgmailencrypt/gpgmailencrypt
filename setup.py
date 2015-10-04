749c1d9c8a563c188de375ca7b1f11af4861e212
Horst Knorr <gpgmailencrypt@gmx.de>
2015-10-04 23:01:37 +0200

    setup.py update

--- setup.py
+++ setup.py
@@ -11,18 +11,24 @@ from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
 from os import path
-
+import re
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the relevant file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
+VERSIONFILE="gpgmailencrypt.py"
+src = open(VERSIONFILE, "rt").read()
+result=re.search("^VERSION=[\"]([\.0-9a-zA-Z]*)[\"]",src,re.M)
+if result:
+	versionstr=result.group(1)
+else:
+    	raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE))
 
 setup(
     name='gpgmailencrypt',
-    version='2.0.0',
-
-    description='an e-mail encryption module and daemon',
+    description='an e-mail encryption module, gateway and daemon',
+    version=versionstr,
     long_description=long_description,
     url='https://github.com/gpgmailencrypt/gpgmailencrypt',
     author='Horst Knorr',
@@ -38,6 +44,7 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
 	"Topic :: Communications :: Email :: Mail Transport Agents",
 	"Topic :: Communications :: Email",
 	"Topic :: Security :: Cryptography",
@@ -45,8 +52,10 @@ setup(
 	"Operating System :: OS Independent",
    ],
 
-    keywords='Email encryption daemon',
-    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    keywords='Email encryption daemon gateway',
+    scripts =["scripts/gme_admin.py","scripts/encryptmaildir.py"],
+    packages=["","mailtemplates","documentation","misc"],
+    package_data={'mailtemplates': ['*/*'],"documentation":["*"],"misc":["*"]},
     # https://packaging.python.org/en/latest/requirements.html
 )
 
