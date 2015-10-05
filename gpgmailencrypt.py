70f4348501e897f102c93cfdb8687bded941a671
Horst Knorr <gpgmailencrypt@gmx.de>
2015-10-05 18:39:12 +0200

    version update

--- gpgmailencrypt.py
+++ gpgmailencrypt.py
@@ -18,7 +18,7 @@ Usage:
 Create a configuration file with "gpgmailencrypt.py -x > ~/gpgmailencrypt.conf"
 and copy this file into the directory /etc
 """
-VERSION="2.1.0lamda"
+VERSION="2.1.0"
 DATE="05.10.2015"
 from configparser import ConfigParser
 import email,email.message,email.mime,email.mime.base,email.mime.multipart,email.mime.application,email.mime.text,smtplib,mimetypes
