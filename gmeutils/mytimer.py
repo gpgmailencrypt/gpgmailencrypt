e59d0a0513e0447a24ad6160fecbb952d840800b
Horst Knorr <gpgmailencrypt@gmx.de>
2015-10-27 20:40:43 +0100

    mytimer and virus_check classes moved to gmeutils

---
+++ gmeutils/mytimer.py
@@ -0,0 +1,150 @@
+from .child import _gmechild
+import threading
+import time
+
+#########
+#_mytimer
+#########
+
+class _mytimer(_gmechild):
+	"""
+	Timer class that can act either as a countdown timer or a periodic revolving 
+	timer.
+	
+	The class will return timer.is_running() == False in case the timer expired.
+	
+	Additionally you can set a your own alarmfunction to do whatever you want
+	when the timer is fired.
+	
+	
+	case 1: A countdown timer
+	
+	timer.set_timer(10,60) 
+	
+	(10 times multiplicated with 60 seconds = 10 minutes)
+	will be called once after 10 minutes
+	
+	
+	case 2: a revolving timer
+	
+	timer.set_timer(0,600,your_alarmfunction)
+	
+	will  your_alarmfunction every 600 seconds
+	
+	
+	case 3: A timer to check for inactivity
+	
+	To get a timer to check inactivity you can do the following:
+	mytimer.start(10,60) 
+	
+	and then in your useraction function call timer.set_alive() in case of 
+	user activity.
+	
+	To check if the the timer already expired check timer.is_running()
+	"""
+
+	def __init__(self,parent=None):
+		_gmechild.__init__(self,parent)
+		self.counter=0
+		self.alarmtime=10
+		self.timer=1
+		self.running=False
+		self.alarmfunc=None
+		self.alarmfuncargs=[]
+		self.kwalarmfuncargs={}
+
+	########
+	#_action
+	########
+
+	def _action(self):
+		if self.counter==1:
+			self._alert()
+		else:
+			if self.counter>0:
+				self.counter-=1
+			else:
+				if self.alarmfunc:
+					self.alarmfunc(*self.alarmfuncargs,**self.kwalarmfuncargs)
+			self._create_timer()
+
+	#######
+	#_alert
+	#######
+
+	def _alert(self):
+		if self.alarmfunc:
+			self.alarmfunc(*self.alarmfuncargs,**self.kwalarmfuncargs)
+		self.running=False
+
+	##############
+	#_create_timer
+	##############
+
+	def _create_timer(self):
+		self.alarm=threading.Timer( self.timer,
+									self._action)
+		self.running=True
+		self.alarm.start()
+
+	
+	###########
+	#is_running
+	###########
+
+	def is_running(self):
+		"""returns True if the timer is running, 
+		returns False after the timer expired"""
+		return self.running
+
+	##########
+	#set_alive
+	##########
+
+	def set_alive(self):
+		"""
+		if the timer is running it increases the duration of the alarm to its
+		original start alarmtime.
+		if the timer isn't running, this function has no effect.
+		"""
+		self.counter=self.alarmtime
+
+	######
+	#start
+	######
+
+	def start(  self,
+				alarmtime=10,
+				timerinterval=1, 
+				alarmfunction=None,
+				alarmargs=(),
+				kwalarmargs={}):
+		"""
+		The timer will be fired after "alarmtime" multiplicated with
+		"timerintervall" in seconds. 
+	
+		if alarmtime is set to 0, it will be an eternal loop, until stop() 
+		is called.You have to set a user defined alarmfunction to use this 
+		option.
+		
+		alarmargs and kwalarmargs are the values for the user defined alarm 
+		function.
+		"""
+		self.alarmtime=alarmtime
+		self.timer=timerinterval
+		self.alarmfunc=alarmfunction
+		self.alarmfuncargs=alarmargs
+		self.kwalarmfuncargs=kwalarmargs
+		self.counter=self.alarmtime
+		self._create_timer()
+
+	#####
+	#stop
+	#####
+
+	def stop(self):
+		"stops a running timer"
+		self.alarm.cancel()
+		self.running=False
+
+
---
+++ gmeutils/viruscheck.py
@@ -0,0 +1,268 @@
+from .child import _gmechild
+from . import virusscanners
+from . import archivemanagers
+import email
+import tempfile
+import os
+
+###########
+#viruscheck
+###########
+
+class _virus_check(_gmechild):
+	
+	def __init__(self,parent):
+		_gmechild.__init__(self,parent)
+		self.debug("viruscheck __init__")
+		self.archivemap={}
+		self.unpacker={}
+		self.virusscanner={}
+		self._search_archivemanager()
+		self._search_virusscanner()
+
+	###########
+	#_mktempdir
+	###########
+
+	def _mktempdir(self,directory=None):
+		return tempfile.mkdtemp(dir=directory)
+
+	#######
+	#_chmod
+	#######
+
+	def _chmod(self,directory):
+		for root, directories, files in os.walk(directory):  
+
+			for d in directories:  
+				pathd=os.path.join(root,d)
+				os.chmod(pathd,0o770)
+
+			for f in files:
+				pathf=os.path.join(root,f)
+				os.chmod(pathf,0o640)
+
+			os.chmod(directory,0o770)
+				
+	#####################
+	#_search_virusscanner
+	#####################
+
+	def _search_virusscanner(self):
+
+		
+		for s in virusscanners.get_virusscannerlist():
+			vscanner=virusscanners.get_virusscanner(scanner=s,parent=self)
+
+			if vscanner!=None:
+				self.virusscanner[s]=vscanner
+				self.log("Virusscanner %s registered"%s)
+				
+		if len(self.virusscanner)==0:
+			self.log("No virusscanners available!","e")
+
+	#######################
+	#_search_archivemanager
+	#######################
+
+	def _search_archivemanager(self):
+
+		for m in archivemanagers.get_managerlist():
+			mngr=archivemanagers.get_archivemanager(m,self.parent)
+
+			if mngr!=None and len(mngr.cmd)>0:
+				self.unpacker[m]=mngr
+				archiveformats=self.unpacker[m].unpackingformats()
+				self.log("Archivemanager %s registered: Filetypes: %s"
+							%(m,archiveformats))
+
+				for f in archiveformats:
+					ff=f.upper()
+
+					try:
+						self.archivemap[ff]
+					except:
+						self.archivemap[ff]=m
+
+	##################
+	#_print_archivemap
+	##################
+
+	def _print_archivemap(self):
+			for f in self.archivemap:
+				print(("Format %s"%f).ljust(20)+
+					"Unpacker %s"%self.archivemap[f]) 
+
+	#############################
+	#check_directory_for_archives
+	#############################
+
+	def check_directory_for_archives(self,directory):
+		self.debug("check_directory_for_archives '%s'"%directory)
+		
+		for root, directories, files in os.walk(directory):  
+			for f in files:
+				pathf=os.path.join(root,f)
+				self.debug("check file %s"%f)
+				archivetype=archivemanagers.get_archivetype(f,"other/other")
+				_unpacker=None
+
+				try:
+					_unpacker=self.archivemap[archivetype]
+				except:
+					pass
+			
+				#self.debug("\nFile %s, is archive %s of type %s,unpacker %s"
+				#			%(f,isarchive,archivetype,_unpacker))
+
+				if archivetype!=None and _unpacker!=None:
+					_u=None
+					
+					try:
+						_u=self.unpacker[_unpacker]
+					except:
+						return
+
+					self.debug("unpack archive %s"%f)
+					subdir=self._mktempdir(directory=directory)
+					newdir=os.path.join(directory,subdir)
+					self.debug("   new dir is %s"%newdir)
+					_u.uncompress_file(pathf,directory=newdir)
+
+					try:
+						self.debug("_remove_mail_from_queue file '%s'"%pathf)
+						os.remove(pathf)
+					except:
+						pass
+
+					self.check_directory_for_archives(newdir)
+
+	##################
+	#unpack_attachment
+	##################
+
+	def unpack_attachment(self,payload,directory):
+			filename = payload.get_filename()
+			fname=os.path.join(directory,filename)
+			contenttype = payload.get_content_type()
+
+			try:
+				with open(fname,"wb") as attachment:
+					attachment.write(payload.get_payload(decode=True))
+			except:
+				self.log("file '%s' could not be stored"%filename)
+				self.log_traceback()
+
+			archivetype=archivemanagers.get_archivetype(filename,contenttype)
+			_unpacker=None
+
+			try:
+				_unpacker=self.archivemap[archivetype]
+			except:
+				pass
+			
+			self.debug("File %s, is archivetype %s,unpacker %s"
+						%(filename,archivetype,_unpacker))
+
+			if archivetype!=None and _unpacker!=None:
+				_u=None
+				
+				try:
+					_u=self.unpacker[_unpacker]
+				except:
+					return
+
+				subdir=self._mktempdir(directory=directory)
+				newdir=os.path.join(directory,subdir)
+				_u.uncompress_file(fname,directory=newdir)
+				self.check_directory_for_archives(newdir)
+
+				if not _u.keep_for_viruscheck():
+					try:
+						self.debug("_remove_mail_from_queue file '%s'"%fname)
+						os.remove(fname)
+					except:
+						self.debug("keep archive %s"%fname)
+						pass
+		
+	#############
+	#unpack_email
+	#############
+
+	def unpack_email(self, mail):
+
+		if isinstance(mail,str):
+			mail=email.message_from_string(mail)
+
+		tmpdir=self._mktempdir()
+		self.debug("has_virus tmpdir '%s'"%tmpdir)
+		_c=0
+
+		for payload in mail.walk():
+			_c+=1
+			is_attachment = payload.get_param(   
+								'attachment', 
+								None, 
+								'Content-Disposition' ) is not None
+			is_inline = payload.get_param( 
+								'inline', 
+								None, 
+								'Content-Disposition' ) is not None
+			contenttype=payload.get_content_type()				
+
+			if is_attachment:
+				self.debug("payload %i"%_c)
+				self.unpack_attachment(payload,tmpdir)
+			elif contenttype=="text/html":
+				self.debug("payload %i"%_c)
+				f=tempfile.NamedTemporaryFile(  mode='wb',
+											delete=False,
+											prefix='content-',
+											suffix=".html",
+											dir=tmpdir)
+				fname = f.name
+				f.write(payload.get_payload(decode=True))
+				f.close()
+
+		self._chmod(tmpdir)
+		return tmpdir
+
+	##########
+	#has_virus
+	##########
+
+	def has_virus(self,mail):
+		self.debug("viruscheck has_virus")
+		description=[]
+
+		if mail==None:
+			return False,description
+
+		if len(self.virusscanner)==0:
+			description.append("No virusscanners available")
+			return False,description
+			
+		directory=self.unpack_email(mail)			
+		result=False
+		
+		for scanner in self.virusscanner:
+			self.debug("Use virus scanner %s ..."%scanner)
+			hasvirus,info=self.virusscanner[scanner].has_virus(directory)
+
+			if hasvirus:	
+				self.debug("_virus_check.has_virus Virus found")
+				result=True
+				description=info
+				break
+			else:
+				self.debug("... %s: no virus found"%scanner)
+
+		try:
+			if not self.parent._DEBUG:
+				shutil.rmtree(directory)
+		except:
+			pass
+	
+		return result,description
+
+
