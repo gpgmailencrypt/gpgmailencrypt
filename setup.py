31458866ac1fda63f330dad817a9b7b8e90d2136
Horst Knorr <gpgmailencrypt@gmx.de>
2015-10-31 16:18:38 +0100

    gpgmailserver works now in external file

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ here = path.abspath(path.dirname(__file__))
 # Get the long description from the relevant file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
-VERSIONFILE="gpgmailencrypt.py"
+VERSIONFILE="gmeutils/version.py"
 src = open(VERSIONFILE, "rt").read()
 result=re.search("^VERSION=[\"]([\.0-9a-zA-Z]*)[\"]",src,re.M)
 if result:
