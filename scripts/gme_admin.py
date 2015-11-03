e9037a9fceb052866778903ae9e17316e6dc8f4e
Horst Knorr <gpgmailencrypt@gmx.de>
2015-11-03 21:33:55 +0100

    code update

--- scripts/gme_admin.py
+++ scripts/gme_admin.py
@@ -3,11 +3,13 @@ import gpgmailencrypt,sys
 host="localhost"
 port=10025
 cl=sys.argv[1:]
+
 try:
 	host=cl[0]
 	port=int(cl[1])
 except:
 	pass
+
 print("Try to connect to %s:%i ..."%(host,port))
 g=gpgmailencrypt.start_adminconsole(host,port)
 
