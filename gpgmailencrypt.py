8c23fa4a90da479b53d20adb46ea67b80af4e5fe
Horst Knorr <gpgmailencrypt@gmx.de>
2015-11-04 21:33:52 +0100

    log output formatted

--- gpgmailencrypt.py
+++ gpgmailencrypt.py
@@ -1058,7 +1058,7 @@ class gme:
 				prefix="Debug"
 
 			t=time.localtime(time.time())
-			_lntxt="Line %i:%s"%(ln,space)
+			_lntxt="Line %s:%s"%(str(ln).ljust(4),space)
 			tm=("%02d.%02d.%04d %02d:%02d:%02d:" % (t[2],t[1],t[0],t[3],
 													t[4],t[5])).ljust(_lftmsg)
 			
