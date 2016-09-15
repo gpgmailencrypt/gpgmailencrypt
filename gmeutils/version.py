39d54e602eb5f0ac9987e48502bc30eda0127bb3
Horst Knorr <gpgmailencrypt@gmx.de>
2016-09-15 21:43:22 +0200

    initial support to create the necessary SQL tables via the admin console

--- gmeutils/version.py
+++ gmeutils/version.py
@@ -1,7 +1,7 @@
 #License GPL v3
 #Author Horst Knorr <gpgmailencrypt@gmx.de>
 VERSION="3.1.0dev"
-DATE="13.09.2016"
+DATE="15.09.2016"
 unicodeerror="replace"
 #database improvements
 #create tables
