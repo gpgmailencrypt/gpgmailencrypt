3caca7cd60329c150dd7f8bcdc19d9567b732f41
Horst Knorr <gpgmailencrypt@gmx.de>
2017-08-12 17:10:29 +0200

    version update

--- gmeutils/version.py
+++ gmeutils/version.py
@@ -1,7 +1,7 @@
 #License GPL v3
 #Author Horst Knorr <gpgmailencrypt@gmx.de>
 VERSION="3.3.0dev"
-DATE="28.07.2017"
+DATE="12.08.2017"
 COPYRIGHTYEAR="2017"
 unicodeerror="replace"
 #330 mail decryption added
