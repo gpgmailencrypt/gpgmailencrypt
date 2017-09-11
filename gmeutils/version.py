fa69b52bf7fb526671094619eabc47a362681505
Horst Knorr <gpgmailencrypt@gmx.de>
2017-09-11 12:37:10 +0200

    documentation update

--- gmeutils/version.py
+++ gmeutils/version.py
@@ -1,9 +1,10 @@
 #License GPL v3
 #Author Horst Knorr <gpgmailencrypt@gmx.de>
 VERSION="3.4.0dev"
-DATE="09.09.2017"
+DATE="11.09.2017"
 COPYRIGHTYEAR="2017"
 unicodeerror="replace"
+#340 pgpinline put attachments in zip container and add pdf content attachment
 #332
 #331 securitylevel=script and pdfpasswordmode=script, starting windows support 
 #330 mail decryption added
