d098e0785f21ce202a532b28094185549662d622
Horst Knorr <gpgmailencrypt@gmx.de>
2017-09-28 19:54:57 +0200

    version update

--- gmeutils/version.py
+++ gmeutils/version.py
@@ -1,7 +1,7 @@
 #License GPL v3
 #Author Horst Knorr <gpgmailencrypt@gmx.de>
-VERSION="3.4.0pre14"
-DATE="25.09.2017"
+VERSION="3.4.0pre15"
+DATE="28.09.2017"
 COPYRIGHTYEAR="2017"
 unicodeerror="replace"
 #340 pgpinline put attachments in zip container and add pdf content attachment
