b01034cddc2fa97d3016a7209cd8287264879232
Horst <horst@knorrnet.de>
2019-06-21 20:38:35 +0200

    version update

--- gmeutils/version.py
+++ gmeutils/version.py
@@ -1,7 +1,7 @@
 #License GPL v3
 #Author Horst Knorr <gpgmailencrypt@gmx.de>
-VERSION="3.4.3"
-DATE="10.06.2019"
+VERSION="3.4.4"
+DATE="21.07.2019"
 COPYRIGHTYEAR="2019"
 unicodeerror="replace"
 #342 pgpmime: encrypt subject
